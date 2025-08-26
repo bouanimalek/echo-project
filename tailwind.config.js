5301a6230da8fbf4063abf89236d5cd2928ca328
bouanimalek <malzovich@gmail.com>
2025-08-26 22:29:13 +0100

    fix: lazyvim suggestions

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// tailwind.config.js (at monorepo root)
+module.exports = {
+  content: ["./apps/**/*.{js,ts,jsx,tsx}", "./packages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
