b3eb12abc5efb537bd2e59bb7c1211fa3293e86e
bouanimalek <malzovich@gmail.com>
2025-08-26 22:30:34 +0100

    feat: design web/app

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,9 +6,11 @@ import { api } from "@workspace/backend/_generated/api";
 export default function Page() {
   const users = useQuery(api.users.getMany);
   return (
-    <div className="flex items-center justify-center min-h-svh">
+    <div className="flex flex-col items-center justify-center min-h-svh">
       <p>Apps/Web</p>
-      <p>{JSON.stringify(users)}</p>
+      <p className="max-w-sm w-full mx-auto">
+        {JSON.stringify(users, null, 2)}
+      </p>
     </div>
   );
 }
