7c6cf89ca62eb95078664c7f2cefeb4077c9db6c
bouanimalek <malzovich@gmail.com>
2025-08-26 15:10:34 +0100

    feat: setting up convex (second part)

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { useQuery } from "convex/react";
-import { api } from "@workspace/backend/convex/_generated";
+import { api } from "@workspace/backend/_generated/api";
 
 export default function Page() {
   const users = useQuery(api.users.getMany);
