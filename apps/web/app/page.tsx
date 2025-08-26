c9a313fd5028438b70ef02dfb2959d244e64d583
bouanimalek <malzovich@gmail.com>
2025-08-26 14:42:48 +0100

    feat: add convex (first part)

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,12 @@
-import { Button } from "@workspace/ui/components/button";
-import { add } from "@workspace/math/add";
+import { useQuery } from "convex/react";
+import { api } from "@workspace/backend/convex/_generated";
+
 export default function Page() {
+  const users = useQuery(api.users.getMany);
   return (
     <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World WebApp</h1>
-        <Button size="sm">Button</Button>
-        <p>{add(1, 2)}</p>
-      </div>
+      <p>Apps/Web</p>
+      <p>{JSON.stringify(users)}</p>
     </div>
   );
 }
