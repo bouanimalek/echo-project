41cf5aef09837b4436af9fa66f9956b973ec44e9
bouanimalek <malzovich@gmail.com>
2025-08-26 22:31:12 +0100

    feat: add convex widget/app

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,16 +1,16 @@
-import { add } from "@workspace/math/add";
-import { Button } from "@workspace/ui/components/button";
-import { Input } from "@workspace/ui/components/input";
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World WidgetApp</h1>
-        <Button size="sm">Button</Button>
-        <p>{add(1, 8)}</p>
-        <Input />
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <p>Apps/Widget</p>
+      <p className="max-w-sm w-full mx-auto">
+        {JSON.stringify(users, null, 2)}
+      </p>
     </div>
   );
 }
